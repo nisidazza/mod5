2f9c228e9a0ced6c0e2b9836114bbdf71b0cb06c
nisidazza <nisida.azzalini@gmail.com>
2015-09-18 12:06:29 +0200

    static member countStudentEnrolled + add StudentEnrolled method

--- ClassesMod5/Student.cs
+++ ClassesMod5/Student.cs
@@ -7,7 +7,6 @@ namespace ClassesMod5
 {
     public class Student
     {
-        public int countStudent;
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime Birthday { get; set; }
@@ -18,25 +17,24 @@ namespace ClassesMod5
         public int ZIP { get; set; }
         public string Country { get; set; }
 
-        public Student(string first, string last, DateTime birthday) // string add1, string add2, string city, string stateprovince, int zip, string country)
+        public Student(string first, string last, DateTime birthday) 
         {
            FirstName = first;
            LastName = last;
            Birthday = birthday;
-           //AddressLine1 = add1;
-           //AddressLine2 = add2;
-           //City = city;
-           //StateProvince = stateprovince;
-           //ZIP = zip;
-           //Country = country;
+           countStudentEnrolled = 0;
         }
 
-        public static int CountStudent()
+        private static int countStudentEnrolled;
+        
+        public Student()
         {
-            Student[] student = new Student[3];
-            int countStudent = student.Count(i => i != null);
+            countStudentEnrolled++;
+        }
 
-            return countStudent;
+        public void StudentEnrolled(Student student)
+        {
+            countStudentEnrolled = countStudentEnrolled + 1;
         }
         
     }
